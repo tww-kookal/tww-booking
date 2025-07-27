1d078899cca5b45b89aab1173adbacd9c14f418b
Pranav S <pranavam.s@gmail.com>
2025-07-27 21:52:44 +0530

    Updates

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const CLIENT_ID = "579207931969-qagsptumrm49d4subjqguf5g3inmep1d.apps.googleusercontent.com";
-export const API_KEY = "AIzaSyCeXvBubOb87QiniO-DCJBviiA8VyJmbw0";
-export const SCOPES = "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets";
-export const SHEET_ID = "1_JjNazWrP6_pI5fxtpHSpK7lttzsdby-l3okOYapQGE";
-export const FOLDER_ID = "1DznyzIPteNm0WGUNKvtOyjtDWjbh_8TI"; // Optional
