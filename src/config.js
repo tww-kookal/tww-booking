2c308e3afd81093b8b0284055af1dc472d0f3150
Pranav <ps@mura.ai>
2025-08-01 18:24:12 +0530

    update FOLDER_ID in config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const CLIENT_ID = "579207931969-qagsptumrm49d4subjqguf5g3inmep1d.apps.goo
 export const API_KEY = "AIzaSyCeXvBubOb87QiniO-DCJBviiA8VyJmbw0";
 export const SCOPES = "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file";
 export const SHEET_ID = "1_JjNazWrP6_pI5fxtpHSpK7lttzsdby-l3okOYapQGE";
-export const FOLDER_ID = "1DznyzIPteNm0WGUNKvtOyjtDWjbh_8TI"; // Optional
+export const FOLDER_ID = "11lLEqPPaWkk27LWTd1YFMnp4nQH_KI55"; // Optional
