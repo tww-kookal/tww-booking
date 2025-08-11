d8339c31f0390af471d7cecc4f3905c5f664b007
Pranav S <pranavam.s@gmail.com>
2025-08-11 18:04:19 +0530

    Updated Viewing from the config

--- src/modules/apiClient.js
+++ src/modules/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
+const API_BASE = import.meta.env.REACT_APP_SERVICE_URL_BASE || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_BASE,
