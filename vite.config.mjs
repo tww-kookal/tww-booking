42060e585d55dc204f94f1d72cf28417aa47e1b0
Pranav S <pranavam.s@gmail.com>
2025-08-11 17:58:12 +0530

    Installed Dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/tww-booking/',
   build: {
     outDir: 'dist',     // Custom output directory inside public
     emptyOutDir: true,
