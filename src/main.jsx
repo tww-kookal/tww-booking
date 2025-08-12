0519a2ac66452ee7c5b41fcc333885fe500ed1fe
Pranav S <pranavam.s@gmail.com>
2025-08-13 02:55:55 +0530

    Udpated Base Path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./styles.css";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/tww-booking/">
         <App />
       </BrowserRouter>
     </AuthProvider>
